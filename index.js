e34ba3ff831724835f256f765c51a0bc04243410
Tuukka Virtanen <duukkis@gmail.com>
2018-11-08 00:09:42 +0200

    Tehtävä 3.4 valmis

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ app.get('/info', (req, res) => {
     res.send(info.numberOfPersons + '<br></br>' + info.date);
 });
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter( person => person.id !== id );
+
+    res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
