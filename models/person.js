7e330587e5a6d766a909e5defaf2347bb972fd3a
Tuukka Virtanen <duukkis@gmail.com>
2018-11-15 22:56:47 +0200

    Tehtävä 3.20 valmis

--- models/person.js
+++ models/person.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
+
+if ( process.env.NODE_ENV !== 'production' ) {
+    const dotenv = require('dotenv');
+    dotenv.config();
+}
+
 const uri = process.env.MONGOLAB_URI;
 
 mongoose.connect(uri, { useNewUrlParser: true });
