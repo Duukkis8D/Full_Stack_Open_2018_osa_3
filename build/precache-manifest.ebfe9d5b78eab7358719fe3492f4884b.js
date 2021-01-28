10a5ead69d04460510fe03849b696e2261e6cd54
Tuukka Virtanen <duukkis@gmail.com>
2021-01-29 01:02:24 +0200

    uibuild

---
+++ build/precache-manifest.ebfe9d5b78eab7358719fe3492f4884b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "91db339e2323cbe68c19",
+    "url": "/static/css/main.d37d5fa3.chunk.css"
+  },
+  {
+    "revision": "91db339e2323cbe68c19",
+    "url": "/static/js/main.91db339e.chunk.js"
+  },
+  {
+    "revision": "2ec9d3e842453390f326",
+    "url": "/static/js/1.2ec9d3e8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b09c6a57d120c171cc5c76c05e4bc524",
+    "url": "/static/media/Nimbus_Mono_L_W03_Regular.b09c6a57.woff2"
+  },
+  {
+    "revision": "ec47226e8084f0c07cb17fde04950139",
+    "url": "/static/media/Nimbus_Mono_L_W03_Regular.ec47226e.woff"
+  },
+  {
+    "revision": "d814696354e4a4514e329e3b22325d48",
+    "url": "/static/media/Nimbus_Sans_L_Regular.d8146963.woff2"
+  },
+  {
+    "revision": "264afca4d54ffc1f3186598b2368b08a",
+    "url": "/static/media/Nimbus_Sans_L_Regular.264afca4.woff"
+  },
+  {
+    "revision": "3d28bb5bd256b37a917eed5d49e491d9",
+    "url": "/index.html"
+  }
+];
